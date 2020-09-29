91bf0d898187bafb7e491a8fdf398c0eec1b9359
Ritu1698 <biswasrituparna83@gmail.com>
2020-09-29 20:22:44 +0530

    [Rituparna]Added: Lastname UT

--- src/test/java/com/bridgelabz/userReg/UserLoginTest.java
+++ src/test/java/com/bridgelabz/userReg/UserLoginTest.java
@@ -58,6 +58,65 @@ public class UserLoginTest {
             boolean validity_check= userLoginMain.FirstNameValidator(firstname);
             Assert.assertEquals(false,validity_check);
         }
+    ////----------------------LASTNAME CHECKING-------------------------
+
+    @Test
+    public void givenLastname_whenValid_shouldReturnTrue()
+    {
+        UserLoginMain userLoginMain = new UserLoginMain();
+        String firstname = "Biswas";
+        boolean validity_check= userLoginMain.LastNameValidator(firstname);
+        Assert.assertEquals(true,validity_check);
+    }
+
+    @Test
+    public void givenLastname_whenlessThan3Char_whenInvalid_shouldReturnFalse()
+    {
+        UserLoginMain userLoginMain = new UserLoginMain();
+        String firstname = "Bi";
+        boolean validity_check= userLoginMain.LastNameValidator(firstname);
+        Assert.assertEquals(false,validity_check);
+    }
+    @Test
+    public void givenLastname_whenAllSmallChar_whenInvalid_shouldReturnFalse()
+    {
+        UserLoginMain userLoginMain = new UserLoginMain();
+        String firstname ="biswas";
+        boolean validity_check= userLoginMain.LastNameValidator(firstname);
+        Assert.assertEquals(false,validity_check);
+    }
+
+    @Test
+    public void givenLastname_whenNumberAdded_whenInvalid_shouldReturnFalse()
+    {
+        UserLoginMain userLoginMain = new UserLoginMain();
+        String firstname ="B1swas";
+        boolean validity_check= userLoginMain.LastNameValidator(firstname);
+        Assert.assertEquals(false,validity_check);
+
+    }
+    @Test
+    public void givenLastname_whenSplCharAdded_whenInvalid_shouldReturnFalse()
+    {
+        UserLoginMain userLoginMain = new UserLoginMain();
+        String firstname ="B!swas";
+        boolean validity_check= userLoginMain.LastNameValidator(firstname);
+        Assert.assertEquals(false,validity_check);
+    }
+    @Test
+    public void givenLastname_whenMiddleCharCaps_whenInvalid_shouldReturnFalse()
+    {
+        UserLoginMain userLoginMain = new UserLoginMain();
+        String firstname ="BiSwas";
+        boolean validity_check= userLoginMain.LastNameValidator(firstname);
+        Assert.assertEquals(false,validity_check);
+    }
+
+
+
+
+
+
 
 
 
